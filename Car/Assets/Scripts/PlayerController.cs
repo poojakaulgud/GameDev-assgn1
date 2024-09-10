1845236fd617f05eb92b6f3afc02afb4c98cff12
Pooja Kaulgud <pooja.kaulgud@somaiya.edu>
2024-09-10 00:14:50 -0700

    initial commit

---
+++ Car/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   private float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
